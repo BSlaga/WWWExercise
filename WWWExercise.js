6ffcdf680bd533bf59d7a0e1fa1f4cfde6315569
BSlaga <148803209+BSlaga@users.noreply.github.com>
2024-02-22 16:07:12 -0500

    Add files via upload

---
+++ WWWExercise.js
@@ -0,0 +1,33 @@
+//1 What is HTTP?
+// A protocal for a browser to communicate with a server
+
+//2 What is a URL?
+//Tells your browser which part of the server to access
+
+//3 What is DNS?
+//Translates a URL to an IP address so the computer knows what server to access
+
+//4 What is a query string?
+//Part of the URL that gives the server info like search words and info submitted in forms
+
+//5 What are two HTTP verbs and how are they different?
+//Get and Post, Get requests info from a server without changing info, Post requests info from a server but changes or creates new info on the server
+
+//6 What is an HTTP request?
+//The browser sends a request to the server to get info from that server, most of the time its a GET request
+
+//7 What is an HTTP response?
+//The server sends a repsonse to the browser with the info you requested.
+
+//8 What is an HTTP header? Give a couple examples of request and response headers you have seen.
+//Makes it so the browser and server can pass additional info within HTTP requests and responses.
+//Request: Language and cookies
+//Response: Set cookies and chaching info
+
+//9 What are the processes that happen when you type "http://somesite.com/some/page.html" into a browser?
+//1. Turns name into IP using DNS
+//2. Browser sends request to server
+//3. Server sends a response back with requested data
+//4. Browser created DOM using data in the response
+
+
